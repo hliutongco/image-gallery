afb3e790acd3fd5746168b77dc6818d958c72064
Helen Liutongco <liutongcoh@gmail.com>
2019-09-28 14:31:56 -0400

    fetched data

--- app/js/index.js
+++ app/js/index.js
@@ -1,15 +1,8 @@
-const express = require('express');
-
-const app = express();
-const port = process.env.PORT || 3000;
-const open = require('open');
-
-app.use(express.static('app'));
-
-// for frontend dependencies:
-// app.use('/scripts', express.static(`${__dirname}/node_modules/`));
-
-app.listen(port, () => {
-  console.log('page loading on localhost:', port);
-  open(`http://localhost:${port}/`)
-});
+fetch('http://my-image-gallery-254305.appspot.com/', {
+  method: 'GET',
+  headers: {
+    "Access-Control-Allow-Origin": '*'
+  }
+})
+.then(res => res.json())
+.then(console.log)
---
+++ app/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const open = require('open');
+const axios = require('axios');
+
+app.use(express.static('app'));
+
+// for frontend dependencies:
+// app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+app.listen(port, () => {
+  console.log('page loading on localhost:', port);
+  open(`http://localhost:${port}/`)
+});
